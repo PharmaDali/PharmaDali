7b289327b88a996f585f994adf98ea9785ebfc31
Denscape <dnmrrdnd@gmail.com>
2026-03-12 03:50:04 +0800

    feat: add Dashboard component and integrate chart.js for data visualization; remove HomePage component

--- pharmadali-admin/src/App.jsx
+++ pharmadali-admin/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import HomePage from "./pages/HomePage";
+import DashBoard from "./pages/DashBoard";
 import SalesReports from "./pages/SalesReports";
 import Settings from "./pages/Settings";
 import PosPage from "./pages/PosPage";
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<DashBoard />} />
           <Route path="sales-reports" element={<SalesReports />} />
           <Route path="settings" element={<Settings />} />
           <Route path="pos" element={<PosPage />} />
