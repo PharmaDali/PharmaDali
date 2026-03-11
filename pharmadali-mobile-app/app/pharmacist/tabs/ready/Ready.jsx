08229412f0d3695b57099512336e1c081a30e7a8
Jamexuu <jamesmercado2209@gmail.com>
2026-03-12 00:19:18 +0800

    specify: create a short notice in the ready tab.

--- pharmadali-mobile-app/app/pharmacist/tabs/ready/Ready.jsx
+++ pharmadali-mobile-app/app/pharmacist/tabs/ready/Ready.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 const Ready = () => {
   return (
-    <View className="flex-1">
-      <Text>Ready</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl font-bold">Ready Tab</Text>
+      <Text className="text-lg">This page will display ready orders.</Text>
+      <Text className="text-sm text-gray-500">The contents and functionalities of this tab will be implemented soon!</Text>
     </View>
   )
 }
